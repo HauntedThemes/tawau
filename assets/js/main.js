8990f7a26f5a25fbc8bf268ae9698a6c9e644f66
Cristian Nistor <Cristian.Nistor@everymatrix.com>
2017-06-26 17:07:40 +0300

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Main JS file for Zwei
+ */
+
+jQuery(document).ready(function($) {
+
+    var config = {
+        
+    };
+
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    
+
+
+});
