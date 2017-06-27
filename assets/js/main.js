f65ce4bebebc14dde6b094f02872eb1ae027776f
Cristian Nistor <Cristian.Nistor@everymatrix.com>
2017-06-27 17:40:12 +0300

    Improved page.hbs, post.hbs, author.hbs, tag.hbs. Added fluidbox. Improved overall styles.

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,17 @@ jQuery(document).ready(function($) {
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     
+    $('.post-content img').each(function(index, el) {
+        if (!$(this).parent().is("a")) {
+            $( "<a href='" + $(this).attr('src') + "' class='zoom'></a>" ).insertAfter( $(this) );
+            $(this).appendTo($(this).next("a"));
+        };
+    });
 
+    $('.zoom').fluidbox();
+
+    $(window).scroll(function() {
+        $('.zoom').fluidbox('close');
+    });
 
 });
