f0bee3932f468662b878b53ef4f04e2e2c308507
Cristian Nistor <Cristian.Nistor@everymatrix.com>
2017-06-29 17:32:15 +0300

    Added social icons. Added SelectedShareText. Start responsive.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
 jQuery(document).ready(function($) {
 
     var config = {
-        
+        'share-selected-text': true,
     };
 
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
@@ -20,8 +20,53 @@ jQuery(document).ready(function($) {
 
     $('.zoom').fluidbox();
 
-    $(window).scroll(function() {
+    $(window).on('scroll', function(event) {
         $('.zoom').fluidbox('close');
     });
+
+    // Initialize shareSelectedText
+    if (config['share-selected-text']) {
+        shareSelectedText('.content-inner .post-content', {
+            sanitize: true,
+            buttons: [
+                'twitter',
+            ],
+            tooltipTimeout: 250
+        });
+    };
+
+    // Position social share buttons inside a single post
+    if ($('.social-share').length) {
+        $(window).scroll(function() {
+            shareButtons();
+        });
+        shareButtons();
+    };
+
+    // Position share buttons
+    function shareButtons(){
+        var contentHolderDistanceTop = $('.content-inner .content-holder').offset().top;
+        var contentHeight = $('#content').outerHeight(true);
+        var contentDistanceTop = $('#content').offset().top - parseInt($('#content').css('marginTop'), 10);
+
+        console.log(contentDistanceTop);
+
+        if ($(window).scrollTop() > (contentHolderDistanceTop - 150) && $(window).scrollTop() < (contentHeight + contentDistanceTop - 150 - $('.content-inner .content-holder .social-share').height())) {
+            $('.social-share').addClass('active');
+            $('.social-share').attr('style', '');
+            console.log($(window).scrollTop());
+            console.log(contentHeight + contentDistanceTop);
+            console.log('-----');
+        }
+        // else if($(window).scrollTop() > (contentHeight + contentDistanceTop - 190)){
+        //     $('.social-share').css({
+        //         position: 'absolute',
+        //         top: contentHeight - 300 + 'px'
+        //     });
+        // }else if($(window).scrollTop() < (contentHolderDistanceTop - 150)){
+        //     $('.social-share').removeClass('active');
+        //     $('.social-share').attr('style', '');
+        // }
+    }
 
 });
